9962860917554ff129b2e1d8938668bc8fe9c431
unknown <julianatonu@gmail.com>
2022-10-11 13:00:29 +0600

    added quiz questions

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 // import QuizDetails from '../QuizDetails/QuizDetails';
+import QuizDetails from './../QuizDetails/QuizDetails';
 
 const Quiz = () => {
 
@@ -13,7 +14,10 @@ const Quiz = () => {
           
             <h1>hey quiz :{topic.data.quetions}</h1>
             {
-                topic.data.questions.map(quiz=>   <li> tonu : {quiz.id}</li>)
+                topic.data.questions.map(quiz=><QuizDetails
+                key ={quiz.id}
+                quiz ={quiz}
+                ></QuizDetails>   )
             }
         </div>
         
--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 
-const QuizDetails = () => {
-    
+const QuizDetails = ({quiz}) => {
+    const {correctAnswer,id, options, question} =quiz
+
     return (
         <div>
+            <h1>{question}</h1>
             
+            <p>{options[0]}</p>
         </div>
     );
 };
