76e8e28a4152050fb1730a0f4f95d7aebf2c7261
unknown <julianatonu@gmail.com>
2022-10-11 10:26:31 +0600

    added topicdetails

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Topic.css'
 const Topic = ({topic}) => {
-
+const {name,logo}=topic
     return (
-        
-        <div></div>
+        <div className='topic row'>
+            <div className='col-6'>
+            <h1>hey topic {topic.id}</h1>
+
+<img src={logo} alt="" />
+<h3>Name:{name}</h3>
+<button>Start Quiz</button>
+            </div>
+        </div>
     );
 };
 
