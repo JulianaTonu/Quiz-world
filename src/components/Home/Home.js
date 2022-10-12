1fc246644aa254b718dd32e2cd3278a0cf383d92
unknown <julianatonu@gmail.com>
2022-10-12 14:51:49 +0600

    edited header

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,8 +13,8 @@ const Home = () => {
             <div className='container header'>
             <img src="https://images.unsplash.com/photo-1516534775068-ba3e7458af70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
             
-            <p>Here you can perticipate Quiz in four different topic.There are some important question about 
-            those topic.you can develop and judge your skill by participate this quiz.Hope you enjoy it.Good Luck.</p>
+            <p>There are some important question about 
+            Four different topics.They are React, JavaScript, Css, Git.You can develop and judge your skill by participate this quiz.Hope you will enjoy it.Good Luck.</p>
             </div>
             </div>
 <div className='col-6 data text-center'>
