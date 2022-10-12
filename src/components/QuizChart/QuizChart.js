75f79718431cf0a49b650a324fdb33efdc7cc1d3
unknown <julianatonu@gmail.com>
2022-10-12 10:35:29 +0600

    added line chart details

--- src/components/QuizChart/QuizChart.js
+++ src/components/QuizChart/QuizChart.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import {  useLoaderData } from 'react-router-dom';
-import ChartDetails from '../ChartDetails/ChartDetails';
+
+import { LineChart, Line, XAxis, YAxis,  Tooltip, Legend, ResponsiveContainer, CartesianGrid } from 'recharts';
 
 const QuizChart = () => {
-    const topics = useLoaderData()
-    console.log('topic',topics.data)
+    const topics = useLoaderData().data
+    // console.log('topic',topics.data)
     return (
         <div>
-            <h1>hey chart</h1>
-
-            {
-    topics.data.map(topic=>
-        <ChartDetails
-        key={topic.id}
-        topic ={topic}
-
+            <h1 className='my-5'>Simple LineChart of Total Quiz</h1>
+            <ResponsiveContainer width="100%" aspect={3}>
+        <LineChart
+          width={500}
+          height={300}
+          data={topics}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
         >
-        </ChartDetails>
-        
-    )
-}
+           <CartesianGrid strokeDasharray="3 3" />
+          {/* <div>{total}</div> */}
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          
+          <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
+      
 
         </div>
     );
--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link} from 'react-router-dom';
-import Quiz from '../Quiz/Quiz';
+// import Quiz from '../Quiz/Quiz';
 
 import './Topic.css'
 
